a69b17970e3d01d09c6c4a7f9fab7297aef4ebc6
Felipe Trova de Araujo <fetrova@gmail.com>
2018-05-18 14:23:03 -0300

    Update home.dart

---
+++ lib/home.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/utils/constants.dart';
+import 'package:whatsapp/sections/camera_screen.dart';
+import 'package:whatsapp/sections/call_screen.dart';
+import 'package:whatsapp/sections/chat_screen.dart';
+import 'package:whatsapp/sections/status_screen.dart';
